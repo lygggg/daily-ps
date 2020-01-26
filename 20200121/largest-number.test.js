c481bea4036e97ea5c7973a4d5b767d214c9ca8c
younggyulee <baayoo93@naver.com>
2020-01-26 19:46:31 +0900

    Solve problem by javascript

--- 20200121/largest-number.test.js
+++ 20200121/largest-number.test.js
@@ -1,15 +1,13 @@
 const solution = (numbers) => {
-console.log(numbers.sort(compare))
-   numbers.sort(compare)[0] === 0 ? '0' : numbers.join('');
+    return numbers.sort(compare)[0] === 0 ? '0' : numbers.join('');
 }
 
 const compare = (a, b) => {
-    console.log((a + '' + b > b + '' + a) ? -1 : 1);
-   return (a + '' + b > b + '' + a) ? -1 : 1
+    return a +''+ b > b + '' + a ? -1 : 1;
 }
 
 test('solution', () => {
-    expect(solution([100,0,2])).toBe('2102100');
+    expect(solution([210,2,100])).toBe('2210100');
     expect(solution([6,10,2])).toBe('6210');
 })
 
