3cbe65b89826e60f2da307fd0ee6244b89655258
younggyulee <baayoo93@naver.com>
2021-08-14 10:21:05 +0900

    상호작용문제

--- 20210812/mutualEvaluation1.test.js
+++ 20210812/mutualEvaluation1.test.js
@@ -1,10 +1,4 @@
 
-// let solution = scores =>
-//     (scores[0].map((_, c) => scores.map(r => r[c])))
-//         .map((s, i) => [...s.splice(i, 1), s])
-//         .map(([m, s]) => Math.min(...s) <= m && m <= Math.max(...s) ? [m, ...s] : s)
-//         .map(s => "FDDCBAA"[Math.max(parseInt(s.reduce((a, c) => a + c) / s.length / 10) - 4, 0)])
-//         .join("")
 const mutualEvaluation = (scores) => {
     console.log(
     (scores[0].map((_, c) => scores.map(r => r[c])))
