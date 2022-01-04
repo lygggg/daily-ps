82d1f71d9d183623f0ca4acfd8831f1bf7033eec
younggyulee <baayoo93@naver.com>
2022-01-04 20:47:21 +0900

    Add: binary tree 문제를 풀었다

---
+++ 20220103/binaryTree.test.js
@@ -0,0 +1,19 @@
+const binaryTree = (arr) => {
+  let num = arr[0];
+  conculate(arr, num, (result = []));
+  console.log(result);
+};
+
+const conculate = (arr, num, result) => {
+  if (num > arr.length) {
+    return;
+  } else {
+    conculate(arr, num * 2, result);
+    conculate(arr, num * 2 + 1, result);
+    result.push(num);
+  }
+};
+
+test("binaryTree", () => {
+  expect(binaryTree([1, 2, 3, 4, 5, 6, 7])).toBe();
+});
