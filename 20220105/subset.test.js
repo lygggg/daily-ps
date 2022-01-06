1ad0dd99335f482af643e9cebf6d2b57ad10dfd1
younggyulee <baayoo93@naver.com>
2022-01-06 22:55:24 +0900

    Add: 부분함수 문제를 다른 방법으로 풀었다

--- 20220105/subset.test.js
+++ 20220105/subset.test.js
@@ -5,29 +5,38 @@
 구해야하는 것
 1부터 N까지의 원소를 갖는 집합의 부분집합을 담은 배열
 
+계획
+1부터 n까지의 배열을 생성한다.
+check라는 이름의 n의 길이만큼 0을 채운 배열을 생성한다. DFS라는 재귀함수를 만들고 끝나는 값은 깊이가 n이상일 경우 리턴해준다.
+dfs 왼쪽 오른쪽을 향하는 두개를 호출하고, 왼쪽으로 갈때는 check[깊이] = 1 오른쪽으로 갈때는 check[깊이] = 0 으로 바꿔준다.
+
 */
 
 const subset = (n) => {
-  let answer = [];
-  let ch = Array.from({ length: n + 1 }, () => 0);
+  const arr = Array(n)
+    .fill()
+    .map((e, i) => i + 1);
+  const check = Array(n).fill(0);
+  DFS(0, arr, check, (result = []));
+  result.pop();
+  return result;
+};
 
-  function DFS(v) {
-    if (v === n + 1) {
-      let tmp = "";
-      for (let i = 1; i <= n; i++) {
-        if (ch[i] === 1) tmp += i;
-      }
-      answer.push(tmp);
-    } else {
-      ch[v] = 1;
-      DFS(v + 1);
-      ch[v] = 0;
-      DFS(v + 1);
-    }
+const DFS = (depth, arr, check, result) => {
+  if (depth === arr.length) {
+    result.push(
+      arr.filter((e, i) => {
+        if (check[i] === 1) {
+          return e;
+        }
+      })
+    );
+    return;
   }
-  DFS(1);
-  console.log(answer);
-  return answer;
+  check[depth] = 1;
+  DFS(depth + 1, arr, check, result);
+  check[depth] = 0;
+  DFS(depth + 1, arr, check, result);
 };
 
 test("subset", () => {
