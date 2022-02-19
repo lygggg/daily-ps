bb230d76e76a17d955996ad4ff090f7804178556
younggyulee <baayoo93@naver.com>
2022-02-19 13:31:40 +0900

    Update overlappingPermutation0218.test.js

--- 20220218/overlappingPermutation0218.test.js
+++ 20220218/overlappingPermutation0218.test.js
@@ -1,10 +1,3 @@
-/*
-1부터 N까지 번호가 적인 구슬이 있습니다. 이 중 중복을 허락하여 M번을 뽑아 일렬로 나열하는 방법을 모두 출력합니다.
-
-계획
-
-*/
-
 const overlappingPermutaion0218 = (N, M) => {
   const arr = Array(N - 1).fill(0);
   const result = [];
