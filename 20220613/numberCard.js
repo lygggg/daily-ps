e9450b6163f925c89b6d7a05655f98f719a0d4a3
younggyulee <baayoo93@naver.com>
2022-06-16 03:07:14 +0900

    Add: 숫자카드 문제 수정

--- 20220613/numberCard.js
+++ 20220613/numberCard.js
@@ -9,19 +9,18 @@ const arr1 = rows1
   .split(" ")
   .map((e) => parseInt(e))
   .sort((a, b) => a - b);
-const M = parseInt(m);
 const arr2 = rows2.split(" ").map((e) => parseInt(e));
 
 const result = [];
 let mid;
-let start = 0;
-let end = M - 1;
 for (let i = 0; i < arr2.length; i++) {
+  let start = 0;
+  let end = N - 1;
   while (start <= end) {
     mid = Math.ceil((start + end) / 2);
     if (arr1[mid] === arr2[i]) {
       result[i] = 1;
-      continue;
+      break;
     }
     if (arr1[mid] > arr2[i]) {
       end = mid - 1;
@@ -29,9 +28,10 @@ for (let i = 0; i < arr2.length; i++) {
     if (arr1[mid] < arr2[i]) {
       start = mid + 1;
     }
-    console.log(result);
   }
-  result[i] = 0;
+  if (!result[i]) {
+    result[i] = 0;
+  }
 }
 
-console.log(result);
+console.log(result.join(" "));
