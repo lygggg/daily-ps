58fa8dd7ebe051df8454649cab44bf5442d1b50d
younggyulee <baayoo93@naver.com>
2022-09-02 22:55:54 +0900

    Add: 콤비네이션

---
+++ JS자료구조 모음/combination.js
@@ -0,0 +1,12 @@
+function combination(arr, num) {
+  const res = [];
+  if (num === 1) return arr.map((v) => [v]);
+
+  arr.forEach((v, idx, arr) => {
+    const rest = arr.slice(idx + 1);
+    const combinations = combination(rest, num - 1);
+    const attach = combinations.map((combination) => [v, ...combination]);
+    res.push(...attach);
+  });
+  return res;
+}
