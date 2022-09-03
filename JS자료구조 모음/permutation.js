be5f6417a36309ad6366fe56d6451de57b0ef50a
younggyulee <baayoo93@naver.com>
2022-09-04 03:01:35 +0900

    Add: 순열 알고리즘

---
+++ JS자료구조 모음/permutation.js
@@ -0,0 +1,12 @@
+function permutation(arr, num) {
+  const res = [];
+  if (num === 1) return arr.map((v) => [v]);
+
+  arr.forEach((v, idx, arr) => {
+    const rest = [...arr.slice(0, idx), ...arr.slice(idx + 1)];
+    const permutations = permutation(rest, num - 1);
+    const attach = permutations.map((permutation) => [v, ...permutation]);
+    res.push(...attach);
+  });
+  return res;
+}
