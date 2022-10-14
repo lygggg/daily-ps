a6c6a6b99ad8644697711eff1d7ebbe316599af1
leeyounggyu <baayoo93@naver.com>
2022-10-15 07:57:57 +0900

    Update delivery.test.js

--- 20220825/delivery.test.js
+++ 20220825/delivery.test.js
@@ -1,49 +1,72 @@
 class State {
-  constructor(arr, time) {
+  constructor(arr, time, isVisited) {
     this.arr = arr;
     this.time = time;
+    this.isVisited = isVisited;
   }
 }
 
 const delivery = (N, road, K) => {
-  const arr = Array(N + 1).fill(Number.MAX_SAFE_INTEGER);
-  const lines = Array.from(Array(N + 1), () => []);
-
-  road.forEach((value) => {
-    let [a, b, c] = value;
-    lines[a].push({ to: b, cost: c });
-    lines[b].push({ to: a, cost: c });
+  const result = new Set();
+  const list = Array.from(Array(N), () => []);
+  road.map(([u, v, time]) => {
+    list[u - 1].push([v - 1, time]);
+    list[v - 1].push([u - 1, time]);
   });
 
-  let queue = [{ to: 1, cost: 0 }];
-  arr[1] = 0;
+  const dfs = (arr, list) => {
+    // 완전 탐색 자료구조 - DFS(Stack), BFS(Queue)
+    // 앞으로 탐색해야할 상태를 담고 있음
+    const stack = [];
+    const isVisited = Array(N).fill(0);
+    isVisited[0] = true;
+    // 초기 상태 삽입
+    stack.push(new State(arr, 0, isVisited));
+    // 문제에서 주어진 크기에 따라 방문 검사 배열 정의
+    // State가 포함하는 변수의 개수에 따라 차원의 수가 결정됨
 
-  while (queue.length) {
-    let { to } = queue.pop();
-
-    lines[to].forEach((next) => {
-      if (arr[next.to] > arr[to] + next.cost) {
-        arr[next.to] = arr[to] + next.cost;
-        queue.push(next);
+    // 탐색할 상태가 남아있는 동안 계속 검사
+    while (stack.length !== 0) {
+      // 현재 방문할 상태
+      const { arr, time, isVisited } = stack.pop();
+      // state에 대한 처리
+      for (const nextState of arr) {
+        if (isVisited[nextState[0]]) {
+          continue;
+        }
+        if (time + nextState[1] <= K) {
+          result.add(nextState[0]);
+        } else {
+          continue;
+        }
+        // 방문 처리
+        // 완전 탐색 자료구조에 넣어줬다.
+        const copyIsVisited = isVisited.slice();
+        copyIsVisited[nextState[0]] = true;
+        stack.push(
+          new State(list[nextState[0]], time + nextState[1], copyIsVisited)
+        );
       }
-    });
-  }
+    }
+  };
 
-  return arr.filter((item) => item <= K).length;
+  dfs(list[0], list);
+  return result.size + 1;
 };
 test("delivery", () => {
   expect(
     delivery(
-      5,
+      6,
       [
         [1, 2, 1],
-        [2, 3, 3],
-        [5, 2, 2],
-        [1, 4, 2],
-        [5, 3, 1],
-        [5, 4, 2],
+        [1, 3, 2],
+        [2, 3, 2],
+        [3, 4, 3],
+        [3, 5, 2],
+        [3, 5, 3],
+        [5, 6, 1],
       ],
-      3
+      4
     )
   ).toBe(4);
 });
